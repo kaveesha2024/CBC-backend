4252d8adbbb0fede99e5b73918bcf4efc194a839
Kaveesha Sashen <sanaakaveesha@gmail.com>
2025-04-18 08:42:11 +0530

    configured and finished view products controller.

--- controllers/Product/viewProducts/ViewProductsController.js
+++ controllers/Product/viewProducts/ViewProductsController.js
@@ -1,6 +1,12 @@
-// import ProductModel from "../../../models/Product/ProductModel.js";
+import ProductModel from "../../../models/Product/ProductModel.js";
 
 const viewProductsController = async (req, res) => {
-    console.log("viewProductsController");
+    try {
+        const response = await ProductModel.find();
+        res.status(200).json(response);
+    }
+    catch (error) {
+        res.status(500).json(error);
+    }
 };
 export default viewProductsController;
