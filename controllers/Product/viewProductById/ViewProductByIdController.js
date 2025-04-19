dc5eee27f7f62383bf926cb86a22ed74a4335ebc
Kaveesha Sashen <sanaakaveesha@gmail.com>
2025-04-19 07:49:35 +0530

    formatted code.

--- controllers/Product/viewProductById/ViewProductByIdController.js
+++ controllers/Product/viewProductById/ViewProductByIdController.js
@@ -1,88 +1,56 @@
 import ProductModel from "../../../models/Product/ProductModel.js";
 
 const viewProductByIdController = async (req, res) => {
-    if (!req.user || !req.user.isAdmin){
-        try {
-            const productId = req.query.productid;
-            if (!productId) {
-                res.status(401).json({
-                    message: "Product id is missing!",
-                });
-                return;
-            }
-            const response = await ProductModel.findOne({ productId });
-            if (!response){
-                res.status(404).json({
-                    message: "Product not found!",
-                });
-                return;
-            }
-            if (response.isAvailable) {
-                res.status(200).json(response);
-                return;
-            }else {
-                res.status(404).json({
-                    message: "Product not found!",
-                });
-                return;
-            }
-        }
-        catch (error) {
-            res.status(500).json({
-                message: "Internal Server Error",
-            });
-            return;
-        }
+  if (!req.user || !req.user.isAdmin) {
+    try {
+      const productId = req.query.productid;
+      if (!productId) {
+        res.status(401).json({
+          message: "Product id is missing!",
+        });
+        return;
+      }
+      const response = await ProductModel.findOne({ productId });
+      if (!response) {
+        res.status(404).json({
+          message: "Product not found!",
+        });
+        return;
+      }
+      if (response.isAvailable) {
+        res.status(200).json(response);
+        return;
+      } else {
+        res.status(404).json({
+          message: "Product not found!",
+        });
+        return;
+      }
+    } catch (error) {
+      res.status(500).json({
+        message: "Internal Server Error",
+      });
+      return;
     }
+  }
 
-    if (req.user.isAdmin) {
-        const productId = req.query.productid;
-        if (!productId) {
-            res.status(401).json({
-                message: "Product id is missing!",
-            });
-            return;
-        }
-        try {
-            const response = await ProductModel.findOne({ productId });
-            res.status(200).json(response);
-        }
-        catch (error) {
-            res.status(500).json({
-                message: "Internal Server Error",
-            })
-        }
+  if (req.user.isAdmin) {
+    const productId = req.query.productid;
+    if (!productId) {
+      res.status(401).json({
+        message: "Product id is missing!",
+      });
+      return;
     }
-    // else {
-    //     try {
-    //         const productId = req.query.productid;
-    //         if (!productId) {
-    //             res.status(401).json({
-    //                 message: "Product id is missing!",
-    //             });
-    //             return;
-    //         }
-    //         const response = await ProductModel.findOne({ productId });
-    //         if (!response){
-    //             res.status(404).json({
-    //                 message: "Product not found!",
-    //             });
-    //             return;
-    //         }
-    //         if (response.isAvailable) {
-    //             res.status(200).json(response);
-    //         }else {
-    //             res.status(404).json({
-    //                 message: "Product not found!",
-    //             });
-    //         }
-    //     }
-    //     catch (error) {
-    //         res.status(500).json({
-    //             message: "Internal Server Error",
-    //         })
-    //     }
-    // }
+    try {
+      const response = await ProductModel.findOne({ productId });
+      res.status(200).json(response);
+    } catch (error) {
+      res.status(500).json({
+        message: "Internal Server Error",
+      });
+    }
+  }
 
 };
 export default viewProductByIdController;
--- controllers/Product/viewProducts/ViewProductsController.js
+++ controllers/Product/viewProducts/ViewProductsController.js
@@ -1,35 +1,34 @@
 import ProductModel from "../../../models/Product/ProductModel.js";
 
 const viewProductsController = async (req, res) => {
-    if (!req.user) {
-        try {
-            const response = await ProductModel.find({isAvailable: true});
-            res.status(200).json(response);
-        }catch (error) {
-            res.status(500).json({
-                message: "Internal Server Error!",
-            })
-        }
+  if (!req.user) {
+    try {
+      const response = await ProductModel.find({ isAvailable: true });
+      res.status(200).json(response);
+    } catch (error) {
+      res.status(500).json({
+        message: "Internal Server Error!",
+      });
     }
-    if (req.user.isAdmin) {
-        try {
-            const response = await ProductModel.find();
-            res.status(200).json(response);
-        }
-        catch (error) {
-            res.status(500).json({
-                message: "Internal Server Error!",
-            });
-        }
-    }else {
-        try {
-            const response = await ProductModel.find({isAvailable: true});
-            res.status(200).json(response);
-        }catch (error) {
-            res.status(500).json({
-                message: "Internal Server Error!",
-            })
-        }
+  }
+  if (req.user.isAdmin) {
+    try {
+      const response = await ProductModel.find();
+      res.status(200).json(response);
+    } catch (error) {
+      res.status(500).json({
+        message: "Internal Server Error!",
+      });
     }
+  } else {
+    try {
+      const response = await ProductModel.find({ isAvailable: true });
+      res.status(200).json(response);
+    } catch (error) {
+      res.status(500).json({
+        message: "Internal Server Error!",
+      });
+    }
+  }
 };
 export default viewProductsController;
