1470d6029029f676298d84e00657e11857fcaad9
Kaveesha Sashen <sanaakaveesha@gmail.com>
2025-05-18 16:32:18 +0530

    changed viewProduct vontrollers respones format.

--- controllers/Product/viewProducts/ViewProductsController.js
+++ controllers/Product/viewProducts/ViewProductsController.js
@@ -1,20 +1,13 @@
 import ProductModel from "../../../models/Product/ProductModel.js";
 
 const viewProductsController = async (req, res) => {
-  // if (!req.user) {
-  //   try {
-  //     const response = await ProductModel.find({ isAvailable: true });
-  //     res.status(200).json(response);
-  //   } catch (error) {
-  //     res.status(500).json({
-  //       message: "Internal Server Error!",
-  //     });
-  //   }
-  // }
   if (req.user?.isAdmin) {
     try {
       const response = await ProductModel.find();
-      res.status(200).json(response);
+      res.json({
+        status: 200,
+        message: response,
+      });
     } catch (error) {
       res.status(500).json({
         message: "Internal Server Error!",
@@ -25,7 +18,10 @@ const viewProductsController = async (req, res) => {
 
     try {
       const response = await ProductModel.find({ isAvailable: true });
-      res.status(200).json(response);
+      res.json({
+        status: 200,
+        message: response,
+      });
     } catch (error) {
       res.status(500).json({
         message: "Internal Server Error!",
